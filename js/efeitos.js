3641f11d8f6e7f45e9b6296aaefe7eb60e09fdf0
Lorenna <Miinzy@users.noreply.github.com>
2017-07-16 19:49:16 -0300

    Update
    Update 1

---
+++ js/efeitos.js
@@ -0,0 +1,36 @@
+	$(document).ready(function(){
+		$("#logotipo").on("mouseover",function(){
+
+			$("#banner h1").addClass("efeito");
+
+		}).on("mouseout", function(){
+
+			$("#banner h1").removeClass("efeito");
+
+		});
+		$("#input-search").on("focus", function(){
+
+				$("li.search").addClass("ativo");
+
+		}).on("blur", function(){
+
+				$("li.search").removeClass("ativo");				
+
+		});
+		$('.owl-carousel').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:4
+		        }
+		    }
+})
+	});
